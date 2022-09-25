598f44bfddea44ddc3569f1db2413c1d6992a426
mettew0585 <mettew0585@naver.com>
2022-09-26 00:52:14 +0900

    just fine

---
+++ app/src/main/java/com/example/test2/GameScreen.kt
@@ -0,0 +1,18 @@
+package com.example.test2
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameScreen : AppCompatActivity() {
+    private val images = arrayOf(R.drawable.character1,R.drawable.character2
+        ,R.drawable.character3,R.drawable.character4,R.drawable.character5)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_screen)
+
+        val bitmap = Bitmap.createBitmap(30,40,Bitmap.Config.ARGB_8888)
+    }
+}
