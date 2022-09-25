a030cd91802489cca05fd57387bdecd1933bc302
mettew0585 <mettew0585@naver.com>
2022-09-26 01:02:04 +0900

    just fine

--- app/src/main/java/com/example/test2/GameScreen.kt
+++ app/src/main/java/com/example/test2/GameScreen.kt
@@ -1,6 +1,8 @@
 package com.example.test2
 
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -14,5 +16,8 @@ class GameScreen : AppCompatActivity() {
         setContentView(R.layout.activity_game_screen)
 
         val bitmap = Bitmap.createBitmap(30,40,Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        canvas.drawColor(Color.WHITE)
+
     }
 }
